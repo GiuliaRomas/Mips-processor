950048d2f515cfa599a326f0242e9e7ead2db1bb
Giulia <49544670+GiuliaRomas@users.noreply.github.com>
2023-08-26 12:34:00 -0300

    Create mux2_1.vhd

---
+++ codes/mux2_1.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity mux2_1 is
+	generic(tam: integer);
+	port(A, B:     in  std_logic_vector(tam-1 downto 0); -- operandos
+		  Selector: in  std_logic; -- operacao
+		  MuxOut:   out std_logic_vector(tam-1 downto 0)); -- saida
+end mux2_1;
+
+architecture synth of mux2_1 is
+begin
+	
+	with Selector select
+	MuxOut <= A  when '0',
+				 B  when '1';
+	
+end synth;
