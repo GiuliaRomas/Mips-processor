9dbdedc2066c841386e5402dd6a3c0b93e1b1c5b
Giulia <49544670+GiuliaRomas@users.noreply.github.com>
2023-08-26 12:30:27 -0300

    Create PC.vhd

---
+++ codes/PC.vhd
@@ -0,0 +1,23 @@
+library ieee; 
+use ieee.std_logic_1164.all;
+
+entity PC is
+	port(din : in  std_logic_vector(31 downto 0);  -- entrada
+		  clk : in  std_logic;                      -- clock
+		  clr : in  std_logic;                      -- clear
+		  dout: out std_logic_vector(31 downto 0)); -- saída
+end PC;
+
+architecture synth of PC is
+begin
+	process(clk, clr, din)
+	begin  
+	
+		if (clr = '1') then         -- se clear = 1
+			dout <= x"00000000";		 -- a saida recebe 0
+		elsif rising_edge(clk) then -- na borda de subida do clock
+			dout <= din;				 -- a saida recebe a entrada
+		end if;
+		
+	end process;
+end synth;
