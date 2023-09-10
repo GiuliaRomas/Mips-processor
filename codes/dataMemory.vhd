794b0c6e4ecedae8e6cf0dc6d7c9b26db799143a
Giulia <49544670+GiuliaRomas@users.noreply.github.com>
2023-09-10 11:52:15 -0300

    Update dataMemory.vhd

--- codes/dataMemory.vhd
+++ codes/dataMemory.vhd
@@ -4,11 +4,12 @@ use ieee.numeric_std.all;
 
 entity dataMemory is
 	generic(constant addr_b: integer := 5;
-			  constant word  : integer := 31);
+		constant word  : integer := 31);
 	port(WE, clk : in  std_logic;
-		  A		 : in  std_logic_vector(addr_b downto 0);
-		  WD		 : in  std_logic_vector(word   downto 0);
-		  RD 		 : out std_logic_vector(word   downto 0));
+	     MemRead : in  std_logic;
+	     A	     : in  std_logic_vector(addr_b downto 0);
+	     WD	     : in  std_logic_vector(word   downto 0);
+	     RD      : out std_logic_vector(word   downto 0));
 end dataMemory;
 
 architecture synth of dataMemory is 
@@ -24,6 +25,5 @@ begin
 	end process;
 	
 	RD <= data_memory(to_integer(unsigned(A))) when WE = '0';
-	
 	
 end synth;
