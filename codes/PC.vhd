711f16e34768f7a09b9e4304d75e6e5cf4fb6565
Giulia <49544670+GiuliaRomas@users.noreply.github.com>
2023-09-10 11:54:36 -0300

    Update PC.vhd

--- codes/PC.vhd
+++ codes/PC.vhd
@@ -3,9 +3,9 @@ use ieee.std_logic_1164.all;
 
 entity PC is
 	port(din : in  std_logic_vector(31 downto 0);  -- entrada
-		  clk : in  std_logic;                      -- clock
-		  clr : in  std_logic;                      -- clear
-		  dout: out std_logic_vector(31 downto 0)); -- saída
+	     clk : in  std_logic;                      -- clock
+	     clr : in  std_logic;                      -- clear
+	     dout: out std_logic_vector(31 downto 0)); -- saída
 end PC;
 
 architecture synth of PC is
@@ -13,11 +13,10 @@ begin
 	process(clk, clr, din)
 	begin  
 	
-		if (clr = '1') then         -- se clear = 1
-			dout <= x"00000000";		 -- a saida recebe 0
-		elsif rising_edge(clk) then -- na borda de subida do clock
-			dout <= din;				 -- a saida recebe a entrada
+		if (clr = '1') then          -- se clear = 1
+			dout <= x"00000000"; -- a saida recebe 0
+		elsif rising_edge(clk) then  -- na borda de subida do clock
+			dout <= din;	     -- a saida recebe a entrada
 		end if;
-		
 	end process;
 end synth;
