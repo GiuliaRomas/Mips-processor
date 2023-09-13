904f051bc49d375006fe5f7317219ed458110356
Giulia <49544670+GiuliaRomas@users.noreply.github.com>
2023-09-13 09:44:40 -0300

    Update alu_control.vhd

--- codes/alu_control.vhd
+++ codes/alu_control.vhd
@@ -13,14 +13,14 @@ begin
 	process(ALUControl) 
 	begin
 		case ALUControl is
-			when "00" => AluOp <= "0010"; -- lw, sw, addi (add)
-			when "01" => AluOp <= "0110"; -- beq (sub)
+			when "00" => AluOp <= "0000"; -- lw, sw, addi (add)
+			when "01" => AluOp <= "0010"; -- beq (sub)
 			when others => case Funct is 	-- R-type
-				when "100000" => AluOp <= "0010"; -- add
-				when "100010" => AluOp <= "0110"; -- sub
-				when "100100" => AluOp <= "0000"; -- and
-				when "100101" => AluOp <= "0001"; -- or
-				when "101010" => AluOp <= "0111"; -- slt
+				when "100000" => AluOp <= "0000"; -- add
+				when "100010" => AluOp <= "0010"; -- sub
+				when "100100" => AluOp <= "0100"; -- and
+				when "100101" => AluOp <= "0101"; -- or
+				when "101010" => AluOp <= "1010"; -- slt
 				when  others  => AluOp <= "XXXX";
 			end case;
 		end case;
