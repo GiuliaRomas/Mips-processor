21aca9f594eff7b8f212ebe88f4c8e1845b43d39
Giulia <49544670+GiuliaRomas@users.noreply.github.com>
2023-10-12 10:30:19 -0300

    Update dataPath.vhd

--- codes/dataPath.vhd
+++ codes/dataPath.vhd
@@ -3,28 +3,28 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity dataPath is
-	port(clk, clr: in std_logic;
-		  PC		        : buffer std_logic_vector(31 downto 0);
-		  WriteData, ALUResult	: buffer std_logic_vector(31 downto 0);
-		  AluOp     		     : in std_logic_vector(3  downto 0);
-		  Instr, ReadData	     : in std_logic_vector(31 downto 0);
-		  RegDst, Jump, PCSrc	     : in std_logic;
-		  MemtoReg, MemRead	     : in std_logic;
-		  MemWrite, ALUSrc, RegWrite : in std_logic;
+	port(clk, clr						  : in 	  std_logic;
+		  PC		 						  : buffer std_logic_vector(31 downto 0);
+		  WriteData, ALUResult		  : buffer std_logic_vector(31 downto 0);
+		  AluOp     					  : in 	  std_logic_vector(3  downto 0);
+		  Instr, ReadData				  : in 	  std_logic_vector(31 downto 0);
+		  RegDst, Jump, PCSrc		  : in 	  std_logic;
+		  MemtoReg, MemRead			  : in 	  std_logic;
+		  ALUSrc, RegWrite 			  : in 	  std_logic;
 		  Zero: out std_logic);		  
 end dataPath;
 
 architecture synth of dataPath is 
 	signal PCPlus4, PCmuxout1, PCmuxout2, PCBranch, PCJump: std_logic_vector(31 downto 0);
-	signal WriteReg: std_logic_vector(4  downto 0);
-	signal SignImm:  std_logic_vector(31 downto 0);
+	signal WriteReg:		 		std_logic_vector(4  downto 0);
+	signal SignImm, imm: 		std_logic_vector(31 downto 0);
 	signal SrcA, SrcB, Result: std_logic_vector(31 downto 0);
 	
 begin
 	------- componentes -------
 	
 	reg_file: entity work.register_file(synth)
-	port map(A1 => Instr(25 downto 21), A2 => Instr(20 downto 16), A3 => WriteReg, WD3 => Result, WE3 => RegWrite, RD1 => SrcA, RD2 => WriteData, clk => clk);
+	port map(A1 => Instr(25 downto 21), A2 => Instr(20 downto 16), A3 => WriteReg, WD3 => Result, WE3 => RegWrite, clr => clr, RD1 => SrcA, RD2 => WriteData, clk => clk);
 	
 	ula: entity work.ula_mips(synth)
 	port map(A => SrcA, B => SrcB, Zero => Zero, Result => AluResult, AluOp => AluOp);
@@ -61,11 +61,13 @@ begin
 	
 	------- adder pcplus4 -------
 	adder_pcplus: entity work.adder(synth)
-	generic map(tam1 => 32, tam2 => 3) port map(A => PC, B => "100", AddOut => PCPlus4);
+	generic map(tam1 => 32, tam2 => 4) port map(A => PC, B => "0100", AddOut => PCPlus4); 
 	
 	------- adder pcbranch ------
+	imm <= SignImm(29 downto 0) & "00";
+	
 	adder_pcbranch: entity work.adder(synth)
-	generic map(tam1 => 32, tam2 => 32) port map(A => SignImm(29 downto 0) & "00", B => PCPlus4 , AddOut => PCBranch);
+	generic map(tam1 => 32, tam2 => 32) port map(A => imm , B => PCPlus4 , AddOut => PCBranch);
 	
 	
 	---------- pc jump ----------
