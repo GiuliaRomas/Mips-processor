30048851478a9f93bdd69cf65da3d04221ac4565
Giulia <49544670+GiuliaRomas@users.noreply.github.com>
2023-10-12 10:29:35 -0300

    Update control_unit.vhd

--- codes/control_unit.vhd
+++ codes/control_unit.vhd
@@ -3,11 +3,11 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity control_unit is
-	port(Opcode, Funct 		    : in  std_logic_vector(5 downto 0);
-		  Zero			    : in  std_logic;								
-		  AluOp		 	    : out std_logic_vector(3 downto 0);
-		  RegDst, Jump, PCSrc	    : out std_logic;
-		  MemtoReg, MemRead 	    : out std_logic;
+	port(Opcode, Funct 			 	 : in  std_logic_vector(5 downto 0);
+		  Zero							 : in  std_logic;								
+		  AluOp		 					 : out std_logic_vector(3 downto 0);
+		  RegDst, Jump, PCSrc		 : out std_logic;
+		  MemtoReg, MemRead 			 : out std_logic;
 		  MemWrite, ALUSrc, RegWrite: out std_logic);		  
 end control_unit;
 
