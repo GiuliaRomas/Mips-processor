d03d4d1699705c4948adae441fbfbb5cc4cc664c
Giulia <49544670+GiuliaRomas@users.noreply.github.com>
2023-10-12 10:32:17 -0300

    Update mips_processor.vhd

--- codes/mips_processor.vhd
+++ codes/mips_processor.vhd
@@ -3,39 +3,33 @@ use ieee.std_logic_1164.all;
 
 
 entity mips_processor is
-	port(clk, clr: in std_logic);
+	port(clk, clr					  : in 	  std_logic;
+		  MemWrite					  : out 	  std_logic;
+		  MemRead					  : buffer std_logic;
+		  Instr, ReadData         : in  	  std_logic_vector(31 downto 0);
+		  ALUResult, PC, WriteData: out 	  std_logic_vector(31 downto 0));
 end mips_processor;
 
 architecture synth of mips_processor is
 	-- sinais de controle --
-	signal ALUControl: std_logic_vector(3 downto 0);
-	signal PCSrc, MemtoReg, Jump, ALUSrc: 	  std_logic; 
-	signal RegDst, RegWrite, MemWrite: std_logic;
+	signal AluOp: std_logic_vector(3 downto 0);
+	signal PCSrc, MemtoReg, Jump, ALUSrc: std_logic; 
+	signal RegDst, RegWrite: std_logic;
 	
 	-- sinais gerais --
 	signal Zero: std_logic;
-	signal PC: std_logic_vector(31 downto 0);
-	signal ALUResult, WriteData, ReadData, Instr: std_logic_vector(31 downto 0);
 begin
-	-- unidade de controle --
-	controlUnit: entity work.control_unit(synth)
-	port map(Funct => Instr(5 downto 0), Opcode => Instr(31 downto 26), 
-	ALUControl => ALUControl, Jump => Jump, PCSrc => PCSrc, Zero => Zero, MemtoReg => MemtoReg,
-	ALUSrc => ALUSrc, RegDst => RegDst, RegWrite => RegWrite, MemWrite => MemWrite);
-	
 	-- datapath --
 	data_path: entity work.dataPath(synth)
 	port map(clk => clk, clr => clr, Zero => Zero, PC => PC, Instr => Instr, 
 	ReadData => ReadData, ALUResult => ALUResult, WriteData => WriteData,
-	ALUControl => ALUControl, Jump => Jump, PCSrc => PCSrc, MemtoReg => MemtoReg, ALUSrc => ALUSrc,
-	RegDst => RegDst, RegWrite => RegWrite, MemWrite => MemWrite);
+	AluOp => AluOp, Jump => Jump, PCSrc => PCSrc, MemtoReg => MemtoReg, ALUSrc => ALUSrc,
+	RegDst => RegDst, RegWrite => RegWrite, MemRead => MemRead);
 	
-	-- memoria de instrucoes --
-	instr_mem: entity work.instructionMemory(synth)
-	port map(A => PC(5 downto 0), RD => Instr);
+	-- unidade de controle --
+	controlUnit: entity work.control_unit(synth)
+	port map(Funct => Instr(5 downto 0), Opcode => Instr(31 downto 26), 
+	AluOp => AluOp, Jump => Jump, PCSrc => PCSrc, Zero => Zero, MemtoReg => MemtoReg,
+	ALUSrc => ALUSrc, RegDst => RegDst, RegWrite => RegWrite, MemWrite => MemWrite, MemRead => MemRead);
 	
-	-- memoria de dados --
-	data_mem: entity work.dataMemory(synth)
-	port map(A => ALUResult(5 downto 0), WD => WriteData, RD => ReadData, WE => MemWrite, clk => clk);
-
 end synth;
