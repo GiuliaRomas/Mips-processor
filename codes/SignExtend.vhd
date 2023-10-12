0bbb08e4da11f60d4f2b47d4f8e3e0e5a8e79d86
Giulia <49544670+GiuliaRomas@users.noreply.github.com>
2023-10-12 10:28:04 -0300

    Update SignExtend.vhd

--- codes/SignExtend.vhd
+++ codes/SignExtend.vhd
@@ -3,8 +3,8 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity SignExtend is
-	port(A:       in  std_logic_vector(15 downto 0);  -- operandos
-	     SignImm: out std_logic_vector(31 downto 0)); -- saida
+	port(A:   	  in  std_logic_vector(15 downto 0);  -- operandos
+		  SignImm: out std_logic_vector(31 downto 0)); -- saida
 end SignExtend;
 
 architecture synth of SignExtend is
@@ -14,6 +14,6 @@ begin
 	
 	with A(15) select 
 	SignImm (31 downto 16) <= x"0000" when '0',
-				  x"FFFF" when '1';
+									  x"FFFF" when others;
 
 end synth;
